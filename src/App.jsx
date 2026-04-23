fe539338879487c1e4cc4338599fa0b91612c1b4
sikandar <sikanderafridi.854@gmail.com>
2026-04-23 12:13:33 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL)
+
+  return (
+    <>
+    <h1>HI IM SIKOO AND I AM LERNING APPWRITE APP</h1>
+    </>
+  )
+}
+
+export default App
