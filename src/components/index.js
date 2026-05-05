9897320eacab06d8eba36437b2c7f3d7e987fa8f
sikandar <sikanderafridi.854@gmail.com>
2026-05-05 14:56:07 +0500

    cont..

--- src/components/index.js
+++ src/components/index.js
@@ -6,7 +6,7 @@ import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE";
 import Signup from "./Signup";
-import Login from "./Login";
+import Login from "./login";
 import Button from "./Button";
 import PostForm from "./post-form/PostForm";
 import PostCard from "./PostCard";
